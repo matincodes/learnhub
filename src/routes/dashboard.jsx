b21680276aabfd381e9fdaac60e2d313a6dd0ff4
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-08-21 00:21:24 +0100

    fix: Update Dashboard component to pass title and paragraph prop

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -1,8 +1,6 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/dashboard')({
-  component: () => <div>Hello /dashboard!</div>
-})m '@/lib/utils'
+import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { useState } from 'react'
+import { cn } from '@/lib/utils'
 import SideNav from '@/components/sideNav/sideNav'
 import TopNav from '@/components/topNav/topNav'
 
@@ -19,7 +17,11 @@ function DashboardComponent() {
 
   return (
     <div className="h-full w-full bg-gray-100 font-montserrat text-[13px] sm:text-[15px]">
-      <TopNav title="Welcome Back, Abisola" paragraph="What would you love to learn today?" />
+      <TopNav
+        title="Welcome Back, Abisola"
+        paragraph="What would you love to learn today?"
+      />
+
       <div
         className={cn(
           'h-fit bg-gray-100 px-2 pb-14 sm:px-4 lg:float-right lg:w-[calc(100%-250px)] lg:px-10 lg:pt-28',
