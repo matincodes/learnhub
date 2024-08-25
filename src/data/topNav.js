79d2b71e47c1bc1f1af1f6493601ff127059eb67
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-08-25 11:51:01 +0100

    refactor: Add topNavData to manage top navigation title

---
+++ src/data/topNav.js
@@ -0,0 +1,24 @@
+export const topNavData = {
+  '/dashboard': {
+    title: 'Welcome Back, Abisola',
+    paragraph: 'What would you love to learn today?',
+  },
+  '/dashboard/courses': {
+    title: 'Courses',
+  },
+  '/dashboard/analytics': {
+    title: 'Analytics',
+  },
+  '/dashboard/leaderboard': {
+    title: 'Leader Board',
+  },
+  '/dashboard/quizzes': {
+    title: 'Quizzes',
+  },
+  '/dashboard/settings': {
+    title: 'Settings',
+  },
+  '/dashboard/profile': {
+    title: 'Profile',
+  },
+}
