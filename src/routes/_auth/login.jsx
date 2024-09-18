67f7c04a4e5b5f6a7308df7938ef7b6f8bb1d4e5
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-09-18 16:19:05 +0100

    refactor: Remove shadcn form component and update related imports

--- src/routes/_auth/login.jsx
+++ src/routes/_auth/login.jsx
@@ -1,136 +1,148 @@
-import { createFileRoute } from '@tanstack/react-router'
 import { Input } from '@/components/ui/input'
-import { Link } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
+import { useForm } from 'react-hook-form'
 
 export const Route = createFileRoute('/_auth/login')({
   component: () => Login(),
 })
 
-const Login = () => (
-  <div className="relative grid p-[20px]">
-    <div className="flex h-[90lvh] w-full overflow-hidden rounded-l-xl">
-      <div className="relative hidden basis-[45%] place-content-center items-center overflow-hidden bg-[#D8DCE4] lg:flex">
-        {/* Logo */}
-        <img
-          src="/assets/learnhub-logo.svg"
-          alt="Logo"
-          className="absolute left-0 top-0 m-[15px] w-[150px]"
-        />
-        {/* Logo */}
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    // formState: { errors },
+  } = useForm()
 
-        {/* Top Right Image */}
-        <img
-          src="/assets/mockups/login_bottom_mockup.svg"
-          alt="MockupImage"
-          className="absolute -right-8 -top-7 w-[280px]"
-        />
-        {/* Top Right Image */}
+  const onSubmit = data => console.log(data)
 
-        <h3 className="w-[90%] font-montserrat text-[45px] font-bold text-normal_green">
-          A Platform to <br /> Empower and upskill
-        </h3>
-
-        {/* Bottom Left Image */}
-        <img
-          src="/assets/mockups/login_bottom_mockup.svg"
-          alt="MockupImage"
-          className="absolute bottom-0 left-0 w-[280px]"
-        />
-        {/* Bottom Left Image */}
-      </div>
-
-      <div className="flex basis-[100%] flex-col place-content-center lg:basis-[55%] lg:flex-row lg:items-center">
-        {/* Logo */}
-        <div className="flex place-content-center lg:hidden">
+  return (
+    <div className="relative grid p-[20px]">
+      <div className="flex h-[90lvh] w-full overflow-hidden rounded-l-xl">
+        <div className="relative hidden basis-[45%] place-content-center items-center overflow-hidden bg-[#D8DCE4] lg:flex">
+          {/* Logo */}
           <img
             src="/assets/learnhub-logo.svg"
             alt="Logo"
-            className="w-[150px]"
+            className="absolute left-0 top-0 m-[15px] w-[150px]"
           />
+          {/* Logo */}
+
+          {/* Top Right Image */}
+          <img
+            src="/assets/mockups/login_bottom_mockup.svg"
+            alt="MockupImage"
+            className="absolute -right-8 -top-7 w-[280px]"
+          />
+          {/* Top Right Image */}
+
+          <h3 className="w-[90%] font-montserrat text-[45px] font-bold text-normal_green">
+            A Platform to <br /> Empower and upskill
+          </h3>
+
+          {/* Bottom Left Image */}
+          <img
+            src="/assets/mockups/login_bottom_mockup.svg"
+            alt="MockupImage"
+            className="absolute bottom-0 left-0 w-[280px]"
+          />
+          {/* Bottom Left Image */}
         </div>
-        {/* Logo */}
 
-        <form
-          action=""
-          method="post"
-          className="width-[100%] space-y-9 p-[10px] lg:basis-[60%]"
-        >
-          <h3 className="font-san text-[38px] font-semibold">Log in</h3>
-
-          <div className="space-y-5">
-            {/* Email */}
-            <div className="inputs grid space-y-2">
-              <label
-                htmlFor="email"
-                className="font-san tracking-wide text-[#303031]"
-              >
-                Email address
-              </label>
-              <Input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Email"
-                className="font-san text-[#AAAAAA] placeholder:leading-loose"
-              />
-            </div>
-            {/* Email */}
-
-            {/* Password */}
-            <div className="inputs grid space-y-2">
-              <label
-                htmlFor="password"
-                className="font-san tracking-wide text-[#303031]"
-              >
-                Password
-              </label>
-              <Input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="Enter password"
-                className="font-san text-[#AAAAAA] placeholder:leading-loose"
-              />
-
-              <div className="mt-1 flex items-center justify-between">
-                <div className="flex items-center space-x-1">
-                  <input
-                    type="checkbox"
-                    name="remember_password"
-                    id="remember_password"
-                    className="relative h-5 w-5 appearance-auto accent-normal_green"
-                  />
-                  <label
-                    htmlFor="remember_password"
-                    className="font-san text-[15px]"
-                  >
-                    Remember Password
-                  </label>
-                </div>
-                <p className="font-san text-[15px] text-[#FA5B66]">
-                  Forgot Password?
-                </p>
-              </div>
-            </div>
-            {/* Password */}
+        <div className="flex basis-[100%] flex-col place-content-center lg:basis-[55%] lg:flex-row lg:items-center">
+          {/* Logo */}
+          <div className="flex place-content-center lg:hidden">
+            <img
+              src="/assets/learnhub-logo.svg"
+              alt="Logo"
+              className="w-[150px]"
+            />
           </div>
+          {/* Logo */}
 
-          <button className="w-full rounded-lg bg-normal_green p-[12px] font-san text-[18px] text-white">
-            Log in
-          </button>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="width-[100%] space-y-9 p-[10px] lg:basis-[60%]"
+          >
+            <h3 className="font-san text-[38px] font-semibold">Log in</h3>
 
-          <p className="font-san">
-            Don&apos;t have an account?
-            <Link
-              to={`/signup`}
-              className="font-semibold text-normal_green underline"
+            <div className="space-y-5">
+              {/* Email */}
+              <div className="inputs grid space-y-2">
+                <label
+                  htmlFor="email"
+                  className="font-san tracking-wide text-[#303031]"
+                >
+                  Email address
+                </label>
+                <Input
+                  type="email"
+                  {...register('email', { required: true })}
+                  id="email"
+                  placeholder="Email"
+                  className="font-san placeholder:leading-loose placeholder:text-[#AAAAAA] focus:text-black focus:outline-none focus-visible:ring-black"
+                />
+              </div>
+              {/* Email */}
+
+              {/* Password */}
+              <div className="inputs grid space-y-2">
+                <label
+                  htmlFor="password"
+                  className="font-san tracking-wide text-[#303031]"
+                >
+                  Password
+                </label>
+                <Input
+                  type="password"
+                  {...register('password', { required: true })}
+                  id="password"
+                  placeholder="Enter password"
+                  className="font-san placeholder:leading-loose placeholder:text-[#AAAAAA] focus:text-black focus:outline-none focus-visible:ring-black"
+                />
+
+                <div className="mt-1 flex items-center justify-between">
+                  <div className="flex items-center space-x-1">
+                    <input
+                      type="checkbox"
+                      name="remember_password"
+                      id="remember_password"
+                      className="relative h-5 w-5 appearance-auto accent-normal_green"
+                    />
+                    <label
+                      htmlFor="remember_password"
+                      className="font-san text-[15px]"
+                    >
+                      Remember Password
+                    </label>
+                  </div>
+                  <p className="font-san text-[15px] text-[#FA5B66]">
+                    Forgot Password?
+                  </p>
+                </div>
+              </div>
+              {/* Password */}
+            </div>
+
+            <button
+              type="submit"
+              className="w-full rounded-lg bg-normal_green p-[12px] font-san text-[18px] text-white"
             >
-              {' '}
-              Sign Up{' '}
-            </Link>
-          </p>
-        </form>
+              Log in
+            </button>
+
+            <p className="font-san">
+              Don&apos;t have an account?
+              <Link
+                to={`/signup`}
+                className="font-semibold text-normal_green underline"
+              >
+                {' '}
+                Sign Up{' '}
+              </Link>
+            </p>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
