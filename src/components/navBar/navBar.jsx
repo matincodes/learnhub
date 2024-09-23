3a3a0d0bf14c8f643421e64f9e289dcf05add482
Egbetokun Timilehin <timmyaof02@gmail.com>
2024-09-23 04:46:17 +0100

    Updated & Unfinished

--- src/components/navBar/navBar.jsx
+++ src/components/navBar/navBar.jsx
@@ -1,8 +1,8 @@
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { Link } from '@tanstack/react-router'
 import { Input } from '@/components/ui/input'
-import { Button } from  '@/components/ui/button'
-import { CiSearch  } from "react-icons/ci"
+import { Button } from '@/components/ui/button'
+import { CiSearch } from 'react-icons/ci'
 import { navLinks } from '@/data/NavBar'
 import { FiMenu, FiX } from 'react-icons/fi'
 import {
@@ -19,80 +19,129 @@ import {
 
 const NavBar = () => {
 
-  const openMenu = useRef(null)
+  const openMenu = useRef(null);
 
   const OpenMenu = () =>{
     openMenu.current.style.background === 'orange'
   }
 
   return (
-    
+    <>
       <div className="top-0 sticky flex items-center justify-around p-8 lg:p-5 bg-white z-40">
         <img
           src="/assets/learnhub-logo.svg" 
           className="absolute w-[120px] lg:relative lg:w-[150px]"
         />
-        <FiMenu className="absolute right-4 text-[23px] items-center text-[#374957] lg:hidden" onClick={OpenMenu} cursor={'pointer'}/>
+        <FiMenu className="absolute right-4 text-[23px] items-center text-[#374957] lg:hidden" onClick={OpenMenu} cursor={'pointer'} />
         {/* <FiX className="absolute right-4 text-[23px] items-center text-[#374957] lg:hidden" onClick={OpenMenu} cursor={'pointer'} /> */}
 
-          <div className='lg:p-2 p-8 flex justify-around items-center relative'>
-               <img src='/assets/learnhub-logo.svg' className='lg:relative  absolute lg:w-[150px] w-[120px] ' />
-               <FiMenu className='lg:hidden absolute right-4 text-[23px] text-[#374957]'/>
 
-               <div className="lg:relative absolute left-0 flex basis-[30%] justify-around p-0 items-center">
-                    <Link to='/' className='[&.active]:font-normal text-[16px] font-san lg:flex hidden'>
-                         Home
-                    </Link>
-                    
-                    <DropdownMenu className='font-san'>
-                         <DropdownMenuTrigger asChild className='lg:flex hidden'>
-                         <Button className='font-normal font-san'>Open</Button>
-                         </DropdownMenuTrigger>
-                         <DropdownMenuContent className="w-14 bg-transparent lg:bg-white">
+        {/* NavLinks and SearchBar */}
+        <div className="flex basis-[50%] items-center p-0 lg:relative" ref={openMenu} >
+         
+         <div className="lg:w-[80%] lg:h-0 lg:relative absolute left-0 lg:top-0 top-[64px] flex w-full h-screen bg-[rgba(255,255,255,0.31)] backdrop-blur-sm ">
+         <div className="lg:flex lg:flex-row lg:relative lg:items-center lg:justify-evenly absolute lg:top-0 left-0 bg-white lg:p-[0] lg:space-y-0 w-full flex flex-col pl-6 pb-7 shadow-lg rounded-b-3xl ">
+          <Link
+            to="/"
+            className=" outline-0 font-san text-[16px] lg:flex [&.active]:font-normal bg-white lg:p-0 pl-0 p-4 "
+          >
+            Home
+          </Link>
 
-                              {navLinks.map(links=>(
-                                   <>
-                                   <DropdownMenuSub>
-                                   <DropdownMenuSubTrigger  key={links.id}> {links.name} </DropdownMenuSubTrigger>
-                                        <DropdownMenuPortal>
-                                        <DropdownMenuSubContent className='bg-white'>
-                                        <DropdownMenuLabel className='text-[#848484] font-normal'>Sub Courses</DropdownMenuLabel>
-                                             {links.subcourses.map(item=>(
-                                                  <>
-                                                       <DropdownMenuItem>
-                                                            <Link to={'/'}> {item.name} </Link>
-                                                       </DropdownMenuItem>
-                                                  </>
-                                             ))}         
-                                        </DropdownMenuSubContent>
-                                        </DropdownMenuPortal>
-                                   </DropdownMenuSub>
-                                   </>
-                              ))}
-                         </DropdownMenuContent>
-                    </DropdownMenu>
+          <DropdownMenu className="font-san">
+            <DropdownMenuTrigger asChild className=" lg:flex !ring-0 !outline-none lg:p-0 focus:bg-transparent pt-7 pb-7 rounded-none">
+              <Button className="font-san font-normal text-[16px] justify-start pl-0">Courses</Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-14 bg-white">
+              {navLinks.map(links => (
+                <>
+                  <DropdownMenuSub>
+                    <DropdownMenuSubTrigger key={links.id}>
+                      {' '}
+                      {links.name}{' '}
+                    </DropdownMenuSubTrigger>
+                    <DropdownMenuPortal>
+                      <DropdownMenuSubContent className="bg-white">
+                        <DropdownMenuLabel className="font-normal text-[#848484]">
+                          Sub Courses
+                        </DropdownMenuLabel>
+                        {links.subcourses.map(item => (
+                          <>
+                            <DropdownMenuItem>
+                              <Link to={'/'}> {item.name} </Link>
+                            </DropdownMenuItem>
+                          </>
+                        ))}
+                      </DropdownMenuSubContent>
+                    </DropdownMenuPortal>
+                  </DropdownMenuSub>
+                </>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          <Link
+            to="/"
+            className=" outline-0 font-san text-[16px] lg:flex [&.active]:font-normal bg-white lg:p-0 pl-0 p-4"
+          >
+            Pricing
+          </Link>
 
 
-               <div className="flex items-center basis-[60%] relative">
-                    <label htmlFor="" className='relative lg:border p-[9px] lg:border-r-0 -right-2 rounded-l-md border-[#848484] bg-white cursor-pointer'><CiSearch  className='text-[20px]'/></label>
-                    <Input type="text" placeholder='Search for anything' className="placeholder:text-black placeholder:font-san border-l-0 focus-visible:ring-0 border-[#848484] lg:flex hidden"/>
-               </div>
+          <div className="grid lg:hidden space-y-7 mt-4">
+          <Button asChild className='lg:hidden w-fit'>
+            <Link to="/login" className="font-san bg-[#FAFFFD] border">
+              Log in
+            </Link>
+          </Button>
+          <Button asChild className='lg:hidden bg-normal_green w-fit '>
+            <Link
+              to="/signup"
+              className="rounded-lg font-san text-white shadow-md lg:hidden "
+            >
+              Sign Up
+            </Link>
+          </Button>
+        </div>
 
-               </div>
+        </div>
 
-               <div className=" w-[15%] lg:flex justify-around hidden">
-                    <Button asChild>
-                         <Link to='/login' className='font-san'>Log in</Link>
-                    </Button>
-                    <Button asChild>
-                         <Link to='/signup' className='font-san rounded-none bg-normal_green text-white'>Sign Up</Link>
-                    </Button>
-               </div>
+
+
+
+          </div>
+
+          <div className="lg:relative absolute left-0 flex lg:basis-full items-center lg:border rounded-lg lg:p-1 p-3 space-x-1">
+            <CiSearch className="text-[30px] relative text-[#848484d8]" />
+            <input
+              type="text"
+              placeholder="Search for anything"
+              className="relative text-[16px] p-1 focus:border-none bg-transparent lg:w-full focus:outline-none focus:ring-0 placeholder:font-san placeholder:text-[#383838e8] hidden lg:flex "
+            />
+          </div>
+        </div>
+        {/* NavLinks and SearchBar */}
+
+
+        <div className="hidden w-[15%] justify-around lg:flex">
+          <Button asChild>
+            <Link to="/login" className="font-san bg-[#FAFFFD]">
+              Log in
+            </Link>
+          </Button>
+          <Button asChild>
+            <Link
+              to="/signup"
+              className="rounded-xl bg-normal_green font-san text-white shadow-md"
+            >
+              Sign Up
+            </Link>
+          </Button>
+        </div>
+        
       </div>
-      </div>
-
-     )
+    </>
+  )
 }
-
 
 export default NavBar
