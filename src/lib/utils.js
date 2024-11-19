c3961cb7a1c58b293c4be2cb49233b00863db73f
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-11-19 20:44:11 +0100

    chore: Add util fn

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs) {
   return twMerge(clsx(inputs))
 }
+
+export const isActive = (pathname, link) => {
+  return pathname.split('/').at(-1) === link.split('/').at(-1)
+}
