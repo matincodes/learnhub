83a4fa58d1cb97909afcf847d12217eb368d451e
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-11-27 14:52:41 +0100

    fix: style bug

--- src/routes/(userDashboard)/_dashboardLayout.jsx
+++ src/routes/(userDashboard)/_dashboardLayout.jsx
@@ -2,8 +2,7 @@ import BottomNav from '@/components/bottomNav/bottomNav'
 import SideNav from '@/components/sideNav/sideNav'
 import TopNav from '@/components/topNav/topNav'
 import { cn } from '@/lib/utils'
-import { redirect } from '@tanstack/react-router'
-import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/(userDashboard)/_dashboardLayout')({
   component: DashboardComponent,
@@ -26,7 +25,7 @@ function DashboardComponent() {
 
       <div
         className={cn(
-          'relative h-full overflow-scroll bg-gray-100 px-2 pb-14 max-lg:bottom-24 sm:px-4 lg:float-right lg:h-fit lg:w-[calc(100%-280px)] lg:px-10 lg:pt-28',
+          'relative h-full overflow-scroll bg-gray-100 px-2 pb-14 max-lg:mb-24 sm:px-4 lg:float-right lg:h-fit lg:w-[calc(100%-280px)] lg:px-10 lg:pt-28',
         )}
       >
         <Outlet />
