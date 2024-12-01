eb87ef1b1fe93a3491e4ec601a4f4ca52d03e9bd
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-12-01 15:36:57 +0100

    feat: add menu in bottom nav

--- src/components/bottomNav/bottomNav.jsx
+++ src/components/bottomNav/bottomNav.jsx
@@ -1,3 +1,11 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 import { navLinks } from '@/data/sideNav'
 import { isActive } from '@/lib/utils'
 import { Link, useLocation } from '@tanstack/react-router'
@@ -7,7 +15,7 @@ export default function BottomNav() {
 
   return (
     <nav className="fixed inset-x-0 bottom-0 flex h-24 flex-row items-center justify-around bg-white lg:hidden">
-      {navLinks.map(({ link, name, iconImage }) => {
+      {navLinks.slice(0, 3).map(({ link, name, iconImage }) => {
         return (
           <div key={name} className="flex items-center justify-center p-3">
             <Link
@@ -22,6 +30,19 @@ export default function BottomNav() {
           </div>
         )
       })}
+      <DropdownMenu>
+        <DropdownMenuTrigger className="flex items-center justify-center p-3">
+          <img src="/assets/hamburger.svg" className='font-bold' alt="menu" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className='bg-white'>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </nav>
   )
 }
