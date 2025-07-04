89016857ada7eb795c49148484bc2d1c279c4872
Enang <divinetidy@gmail.com>
2025-07-04 15:49:45 +0100

    working on api

--- src/routes/_auth/signup.jsx
+++ src/routes/_auth/signup.jsx
@@ -27,7 +27,7 @@ function SignUp() {
       toast({
         variant: 'destructive',
         title: 'Password mismatch',
-        description: 'Please make sure your password match.',
+        description: 'Please make sure your passwords match.',
       })
       setIsLoading(false)
       return
