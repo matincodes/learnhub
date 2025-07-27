5423b2891425cd5cd8b3793f8a243eeea671f699
Fawaz Adeniji <fawazadeniji123@gmail.com>
2025-07-27 18:08:16 +0100

    feat: implement dynamic course fetching in CourseList component

--- src/components/manage_course/course_management/courseList.jsx
+++ src/components/manage_course/course_management/courseList.jsx
@@ -1,39 +1,71 @@
+import { adminGetAllCourses } from '@/api/adminApiService'
+import { Skeleton } from '@/components/ui/skeleton'
+import { useEffect, useState } from 'react'
 import CourseCard from '../../cards/CourseCard'
 import Filter from '../filter_courses/filter'
 
-const totalCourse = [
-  {
-    id: 1,
-    src: '/assets/coursesImages/image1.svg',
-    title: 'Web Wizardry: Mastering Frontend Development',
-  },
-  {
-    id: 2,
-    src: '/assets/coursesImages/image2.svg',
-    title: 'Software Development with Javascript and libaries',
-  },
-  {
-    id: 3,
-    src: '/assets/coursesImages/image3.svg',
-    title: 'Full Stack Developer: From Frontend to Backend',
-  },
-  {
-    id: 4,
-    src: '/assets/coursesImages/image4.svg',
-    title: 'Backend Builder: Architecting Scalable APIs',
-  },
-  {
-    id: 5,
-    src: '/assets/coursesImages/image5.svg',
-    title: 'Mobile App Development with React Native',
-  },
-]
 function CourseList() {
+  const [courses, setCourses] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    let ignore = false
+    setCourses([])
+    setLoading(true)
+
+    const fetchCourses = async () => {
+      try {
+        const response = await adminGetAllCourses()
+        if (!ignore) {
+          setCourses(response.data)
+          console.log('Fetched courses:', response.data)
+        }
+      } catch (error) {
+        console.error('Error fetching courses:', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchCourses()
+
+    return () => {
+      ignore = true
+    }
+  }, [])
+
+  if (loading) {
+    return (
+      <div className="mt-[120px] h-[464px] w-full bg-white px-4 py-3">
+        <Filter />
+        <div className="mt-7 grid grid-cols-4 gap-[29px]">
+          {Array.from({ length: 8 }).map((_, index) => (
+            <Skeleton key={index} className="h-[150px] w-full" />
+          ))}
+        </div>
+      </div>
+    )
+  }
+
+  if (courses.length === 0) {
+    return (
+      <div className="mt-[120px] h-[464px] w-full bg-white px-4 py-3">
+        <Filter />
+        <div className="mt-7 flex h-full w-full items-center justify-center">
+          {/* No courses available message */}
+          <div className="items-center text-center text-gray-500">
+            No courses available
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div className="h-[464px] w-full bg-white px-4 py-3 mt-[120px] ">
+    <div className="mt-[120px] h-[464px] w-full bg-white px-4 py-3">
       <Filter />
       <div className="mt-7 grid grid-cols-4 gap-[29px]">
-        {totalCourse.map(item => (
+        {courses.map(item => (
           <CourseCard key={item.id} e={item} />
         ))}
       </div>
