ad6717225319d19fd1d6b6ea9849c9259fd5b6d6
Fawaz Adeniji <fawazadeniji123@gmail.com>
2025-08-02 08:27:37 +0100

    fix: update baseURL in axiosInstance to point to the correct backend

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'https://learnhub.up.railway.app/',
+  baseURL: 'https://learnhub-backend.up.railway.app/',
   headers: {
     'Content-Type': 'application/json',
   },
