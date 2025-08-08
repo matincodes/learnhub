2b1432c4b143a6a4f704f1013cacec4a1a256091
Fawaz Adeniji <fawazadeniji123@gmail.com>
2025-08-08 11:50:20 +0100

    fix: resolve merge conflict in axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,12 +2,9 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
   baseURL: 'https://learnhub-backend.up.railway.app/',
-<<<<<<< HEAD
-=======
   headers: {
     'Content-Type': 'application/json',
   },
->>>>>>> ad6717225319d19fd1d6b6ea9849c9259fd5b6d6
   // withCredentials: true,
 })
 
