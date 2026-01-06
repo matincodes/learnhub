df332bea3d575c89c00b14fe22672cdfdcc93321
Fawaz Adeniji <fawazadeniji123@gmail.com>
2026-01-06 17:19:10 +0100

    refactor: integrate QueryClientProvider into the app rendering for improved data fetching

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import { AuthProvider, useAuth } from '@/context/auth-context'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AdminProvider } from './context/admin-context'
 import { UserProvider } from './context/user-context'
 import { router } from './router'
+
+const queryClient = new QueryClient()
 
 // Render the app
 const rootElement = document.getElementById('root')
@@ -14,13 +17,15 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <AdminProvider>
-        <AuthProvider>
-          <UserProvider>
-            <App />
-          </UserProvider>
-        </AuthProvider>
-      </AdminProvider>
+      <QueryClientProvider client={queryClient}>
+        <AdminProvider>
+          <AuthProvider>
+            <UserProvider>
+              <App />
+            </UserProvider>
+          </AuthProvider>
+        </AdminProvider>
+      </QueryClientProvider>
     </StrictMode>,
   )
 }
